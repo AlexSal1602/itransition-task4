d3824d5bbc91641348cc1c7c9b5a80e05b3db53f
AlexSal1602 <asalbishvili@hotmail.com>
2023-05-29 00:51:57 +0400

    first steps set up server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+// set up server
+const app = express()
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Serveria`)
+})
+
+app.use(express.json())
+
+// connect to mongodb
+mongoose.connect(
+  process.env.MDB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    if (err) return console.error(err)
+    console.log("Connected to mongoDB")
+  }
+)
+
+//set up routes
+app.use("/auth", require("./routers/userRouter"))
